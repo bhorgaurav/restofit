18bff72e2608ef5fad32727e12e25a6e9227ca73
Gaurav R Bhor <bhor.gaurav@gmail.com>
2017-04-07 11:33:25 -0700

    Progress bar and caching added.

--- app/src/main/java/edu/csulb/android/restofit/helpers/PreferenceKeys.java
+++ app/src/main/java/edu/csulb/android/restofit/helpers/PreferenceKeys.java
@@ -2,4 +2,5 @@ package edu.csulb.android.restofit.helpers;
 
 public class PreferenceKeys {
     public static final String YELP_TOKEN = "YELP_TOKEN";
+    public static final String NEAR_YOU = "NEAR_YOU";
 }
