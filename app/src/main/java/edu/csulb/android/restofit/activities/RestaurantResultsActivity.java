6bbf36d24417f01f85757063005db759e32f06fb
Gaurav R Bhor <bhor.gaurav@gmail.com>
2017-05-03 19:38:31 -0700

    Migrated to ButterKnife

--- app/src/main/java/edu/csulb/android/restofit/activities/RestaurantResultsActivity.java
+++ app/src/main/java/edu/csulb/android/restofit/activities/RestaurantResultsActivity.java
@@ -86,22 +86,10 @@ public class RestaurantResultsActivity extends AppCompatActivity {
                 try {
                     if (response.isSuccessful()) {
                         JSONArray array = new JSONObject(response.body().string()).getJSONArray("restaurants");
-                        for (int i = 0; i < array.length(); i++) {
-                            JSONObject restaurant = array.getJSONObject(i).getJSONObject("restaurant");
-                            Restaurant r = new Restaurant();
-                            r.id = restaurant.getInt("id");
-                            r.name = restaurant.getString("name");
-                            r.average_cost = restaurant.getInt("average_cost_for_two");
-                            r.aggregate_rating = restaurant.getJSONObject("user_rating").getString("aggregate_rating");
-                            r.rating_color = restaurant.getJSONObject("user_rating").getString("rating_color");
-                            r.address = restaurant.getJSONObject("location").getString("address");
-                            r.cuisines = restaurant.getString("cuisines");
-                            r.currency = restaurant.getString("currency");
-                            r.longitude = restaurant.getJSONObject("location").getDouble("longitude");
-                            r.latitude = restaurant.getJSONObject("location").getDouble("latitude");
-                            r.is_delivering_now = restaurant.getInt("is_delivering_now");
-                            r.has_online_delivery = restaurant.getInt("has_online_delivery");
-                            restaurants.add(r);
+                        restaurants.clear();
+                        List<Restaurant> newList = Restaurant.parse(array);
+                        if (newList != null) {
+                            restaurants.addAll(newList);
                         }
                         restaurantAdapter = new RestaurantAdapter(restaurants);
                         recyclerViewRestaurants.setAdapter(restaurantAdapter);
--- app/src/main/java/edu/csulb/android/restofit/pojos/Restaurant.java
+++ app/src/main/java/edu/csulb/android/restofit/pojos/Restaurant.java
@@ -1,6 +1,12 @@
 package edu.csulb.android.restofit.pojos;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant implements Serializable {
 
@@ -9,4 +15,31 @@ public class Restaurant implements Serializable {
     public int average_cost, is_delivering_now, has_online_delivery;
     public double longitude, latitude;
     public String name, address, aggregate_rating, currency, cuisines;
+
+    public static List<Restaurant> parse(JSONArray array) {
+        try {
+            List<Restaurant> restaurants = new ArrayList<>(array.length());
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject restaurant = array.getJSONObject(i).getJSONObject("restaurant");
+                Restaurant r = new Restaurant();
+                r.id = restaurant.getInt("id");
+                r.name = restaurant.getString("name");
+                r.average_cost = restaurant.getInt("average_cost_for_two");
+                r.aggregate_rating = restaurant.getJSONObject("user_rating").getString("aggregate_rating");
+                r.rating_color = restaurant.getJSONObject("user_rating").getString("rating_color");
+                r.address = restaurant.getJSONObject("location").getString("address");
+                r.cuisines = restaurant.getString("cuisines");
+                r.currency = restaurant.getString("currency");
+                r.longitude = restaurant.getJSONObject("location").getDouble("longitude");
+                r.latitude = restaurant.getJSONObject("location").getDouble("latitude");
+                r.is_delivering_now = restaurant.getInt("is_delivering_now");
+                r.has_online_delivery = restaurant.getInt("has_online_delivery");
+                restaurants.add(r);
+            }
+            return restaurants;
+        } catch (JSONException e) {
+            return null;
+        }
+
+    }
 }
